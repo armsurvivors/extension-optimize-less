8c0df439eae3fee8be90635fd8306f275f5ef935
Lane Jennison <lane@lane-fu.com>
2024-01-01 21:22:13 +0000

    fixed optmize-less bug

--- optimize-less.sh
+++ optimize-less.sh
@@ -3,10 +3,11 @@ function user_config__050_optimize_less_packages() {
 	add_packages_to_rootfs irqbalance
 }
 
-function post_family_tweaks_050_optimize_less() {
+function post_family_tweaks_bsp__050_optimize_less() {
 
-	display_alert "armbian-hardwarwe-optimize.service" "{EXTENSION}" "info"
+	display_alert "disable armbian-hardware-optimize.service" "${EXTENSION}" "info"
 	RM_OPTIONS="-f"
+	# chroot_sdcard systemctl --no-reload disable armbian-hardware-optimize.service
 	rm ${RM_OPTIONS} "${destination}"/lib/systemd/system/armbian-hardware-optimize.service
 
 	return 0
